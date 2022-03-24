9fd913f3031286aad581ae6627c922a05f945be5
Chinara <nishanova73@mail.ru>
2022-03-24 12:50:36 +0600

    created index view and urls files for main and app views

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+
+class IndexView(View):
+    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
